658df49b15f4f968677be2337633d4912dc4b058
Prabhakaranmoh <abinayaerror46@gmail.com>
2024-10-06 22:00:19 -1200

    add program

--- python/ClassOveriding.py
+++ python/ClassOveriding.py
@@ -16,4 +16,25 @@ my_cat = Cat()
 
 my_animal.make_sound()  
 my_dog.make_sound()     
-my_cat.make_sound()     
+my_cat.make_sound()    
+
+
+class car:
+    def make_sound(self):
+        print("Some sound")
+
+class tata(car):
+    def make_sound(self):
+        print("Bark")
+
+class toyato(car):
+    def make_sound(self):
+        print("Meow")
+
+my_cars = car()
+my_tata = tata()
+my_toyato = toyato()
+
+my_cars.make_sound()  
+my_tata.make_sound()     
+my_toyato.make_sound()     
